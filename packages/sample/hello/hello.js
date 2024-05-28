dd6a787d309b4638689eed54552999f0771163bc
techswarn <tech.swarns@gmail.com>
2024-05-28 13:18:51 +0530

    code update

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -4,22 +4,22 @@ function main(event, context) {
   console.log(`---------CONTEXT--------`);
   console.log(context);
 
-  try {
-    fetch("https://go.techenv.dev/api/v1/") // api for the get request
-      .then((response) => {
-        response.json();
-        console.log(response.json());
-      })
-      .then((data) => {
-        console.log(data);
-        return { body: { success: true, data: data }, headers: { foo: "bar" } };
-      });
-  } catch (err) {
-    console.log(err);
-    return { body: { success: false, data: err }, headers: { foo: "bar" } };
-  }
+  // try {
+  //   fetch("https://go.techenv.dev/api/v1/") // api for the get request
+  //     .then((response) => {
+  //       response.json();
+  //       console.log(response.json());
+  //     })
+  //     .then((data) => {
+  //       console.log(data);
+  //       return { body: { success: true, data: data }, headers: { foo: "bar" } };
+  //     });
+  // } catch (err) {
+  //   console.log(err);
+  //   return { body: { success: false, data: err }, headers: { foo: "bar" } };
+  // }
 
-  // return { body: "greeting" };
+  return { body: "greeting" };
 }
 
 exports.main = main;
