b58c714ba85ded3229f4f9fdc431e40a31de7f79
techswarn <tech.swarns@gmail.com>
2024-05-28 11:37:52 +0530

    code update

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -3,7 +3,12 @@ async function main(event, context) {
   console.log(event);
   console.log(`---------CONTEXT--------`);
   console.log(context);
-  return { body: { success: true }, headers: { foo: "bar" } };
+  fetch("https://go.techenv.dev/api/v1/") // api for the get request
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      return { body: { success: true, data: data }, headers: { foo: "bar" } };
+    });
 }
 
 exports.main = main;
